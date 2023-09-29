97e06e37023571ab48aeae083745dd9b9678bc62
acer <acer@TMP249-G2-M>
2023-09-29 15:31:31 +0530

    second commit

---
+++ project1/src/com/tns/programs/Encapsulation.java
@@ -0,0 +1,16 @@
+package com.tns.programs;
+//program to demonstrate encapsulation in java
+public class Encapsulation {
+//datamembers
+	public int serialno;
+	public String name;
+	public int age;
+	
+		//memberfunction
+	
+	public void show() {
+		System.out.println("serialno"+ serialno+ "\t"+ "name"+ "\t"+ "age"+age);
+
+	}
+
+}
---
+++ project1/src/com/tns/programs/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package com.tns.programs;
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Encapsulation ob= new Encapsulation();// creating object
+ob.serialno=33;//accessing the datamember by using dot operator
+ob.name="adi";
+ob.age=22;
+ob.show();
+
+
+
+	}
+
+}
